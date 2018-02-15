6c9e18dd8b00c91c5c5319f4da62a131f1b316c3
root <root@ubuntu.administracio.local>
2018-02-15 15:18:18 +0100

    Canvis en el wsgi per que funcioni amb apache del 192.168.1.131

--- Contingencia/wsgi.py
+++ Contingencia/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/PlaDeContingencia')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Contingencia.settings")
 
