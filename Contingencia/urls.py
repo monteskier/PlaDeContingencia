e788859fb0560b72683c0fa24330170946a85018
root <root@ubuntu.administracio.local>
2018-02-15 17:43:41 +0100

    es canvia el url.py per no posar res

--- Contingencia/urls.py
+++ Contingencia/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('pla/', include('pla.urls')),
+    path('', include('pla.urls')),
     path('admin/', admin.site.urls),
 ]
